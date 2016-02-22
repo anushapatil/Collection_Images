22420f10ad9a71ee1b401937e40cffb85c4e99b5
Anusha Patil <Anusha@danger.local>
2016-02-22 11:08:54 +0530

    Initial Commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Anusha Patil on 22/02/2016.
+//  Copyright © 2016 Anusha Patil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by Anusha Patil on 22/02/2016.
+//  Copyright © 2016 Anusha Patil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
